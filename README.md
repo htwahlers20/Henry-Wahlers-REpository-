a145d876750179f4b0ea56991ea8af99568725dc
htwahlers20 <htwahlers@gmail.com>
2025-09-22 09:35:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AP CSP Sandbox
+Use this sandbox for short coding exercises that you don't need to submit.
---
+++ sandbox.py
@@ -0,0 +1,10 @@
+"""
+AP Computer Science Principles Sandbox
+This is a repo you can use to store small warm-up problems and try out code.
+"""
+
+def main() -> None:
+  pass
+
+if __name__ == "__main__":
+  main()
