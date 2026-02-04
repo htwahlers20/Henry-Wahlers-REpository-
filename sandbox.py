eac6d74033ebed6f6edbbc4c42c68ac94069ef47
htwahlers20 <htwahlers@gmail.com>
2026-02-04 19:00:29 +0000

    updated microbit

--- sandbox.py
+++ sandbox.py
@@ -3,8 +3,54 @@ AP Computer Science Principles Sandbox
 This is a repo you can use to store small warm-up problems and try out code.
 """
 
-def main() -> None:
-  pass
+# Imports go at the top
+from microbit import *
 
-if __name__ == "__main__":
-  main()
+
+
+
+def main() -> None:
+        time: list[int] = []
+        display_interval: int = 3000
+        record_interval: int = 57000
+        recording: bool = False
+        loud: float = float(0)
+        import log
+        log.set_labels("temperature", timestamp=log.SECONDS)
+        
+        
+         
+   
+   
+        while True:
+            if button_a.was_pressed():
+               recording = True
+
+            if button_b.is_pressed():
+               recording = False
+               display.show(Image.NO)
+               sleep(display_interval)
+               display.clear()
+            
+               
+           
+            if recording:
+                log.add({"sound" : loud})
+                display.show(len
+            # record
+                time.append(running_time())
+                
+                if microphone.sound_level() > 10:
+                    loud == microphone.sound_level()
+                    display.show(Image.ANGRY)
+                    log.add({"sound" : loud})
+                display.show(len
+                    
+                    
+                
+                
+                    
+                
+                
+            sleep(3000)
+            display.clear()
